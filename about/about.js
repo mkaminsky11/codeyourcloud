f3365fe7f05f26f7ba40cc66e25bbf7e8919c818
Michael Kaminsky <mkaminsky11@gmail.com>
2014-01-28 09:49:33 -0500

    security improvements

--- about/about.js
+++ about/about.js
@@ -1,11 +1,11 @@
-var connection = new WebSocket('ws://64.207.146.34:8080');
+var connection = new WebSocket('wss://64.207.146.34:8080');
 connection.onopen = function () {
 	console.log("open");
 };
 function comment(){
-	var name = document.getElementById("comment_name").value;
-	var mail = document.getElementById("comment_mail").value;
-	var text = document.getElementById("comment_comment").value;
+	var name = strip(document.getElementById("comment_name").value);
+	var mail = strip(document.getElementById("comment_mail").value);
+	var text = strip(document.getElementById("comment_comment").value);
 	connection.send(JSON.stringify({type: "comment", sender: name, email: mail, comment: text}));
 	document.getElementById("comment_name").value = "";
 	document.getElementById("comment_mail").value = "";
@@ -15,4 +15,7 @@ function resetForm(){
 	document.getElementById("comment_name").value = "";
 	document.getElementById("comment_mail").value = "";
 	document.getElementById("comment_comment").value = "";	
+}
+function strip(string){
+	return string.replace(/(<([^>]+)>)/ig,"");
 }
