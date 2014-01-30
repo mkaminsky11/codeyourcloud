6c438dbf78e08ddc97362e972a61e34867ab4c9a
Michael Kaminsky <mkaminsky11@gmail.com>
2014-01-30 16:01:58 -0500

    various

--- node/server.js
+++
@@ -1,7 +0,0 @@
-var ws  = require('websocket.io');
-var server = ws.listen(3000);
-
-server.on('connection', function (socket) {
-  socket.on('message', function () { });
-  socket.on('close', function () { });
-});
