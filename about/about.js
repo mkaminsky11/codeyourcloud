d1143533f79b349b13d49d660b750b61a420e5d9
Michael Kaminsky <mkaminsky11@gmail.com>
2014-02-05 07:42:05 -0500

    logged in indicator

--- about/about.js
+++ about/about.js
@@ -1,3 +1,32 @@
+/*********
+CHECK IF LOGGED IN
+*********/
+var is_logged_in = false;
+function check_login(){
+	gapi.auth.authorize({'client_id': CLIENT_ID, 'scope': SCOPES.join(' '), 'immediate': true},handleLogin);
+}
+function handleLogin(authResult) {
+	if (authResult) {
+		//ok
+		$("#login_button").html('Start Editing');
+		is_logged_in = true;
+	} 
+	else {
+		//
+		$("#login_button").html('<i class="fa fa-google-plus"></i> | Login with Google');
+	}
+}
+function go(){
+	if(is_logged_in){
+		window.location.href = "https://codeyourcloud.com";
+	}
+	else{
+		handleClientLoad();
+	}
+}
+/*********
+COMMENTS
+*********/
 var connection = new WebSocket('wss://codeyourcloud.com:8080');
 connection.onopen = function () {
 	console.log("open");
@@ -19,3 +48,39 @@ function resetForm(){
 function strip(string){
 	return string.replace(/(<([^>]+)>)/ig,"");
 }
+/*******
+REDIRECT
+*******/
+var CLIENT_ID = '953350323460-0i28dhkj1hljs8m9ggvm3fbiv79cude6.apps.googleusercontent.com';
+		//var SCOPES = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/userinfo.email','https://www.googleapis.com/auth/userinfo.profile'];
+		var SCOPES = ['https://www.googleapis.com/auth/drive.install','https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/userinfo.profile'];
+function handleClientLoad() {
+	checkAuth();
+}	
+function checkAuth() {
+	gapi.auth.authorize({'client_id': CLIENT_ID, 'scope': SCOPES.join(' '), 'immediate': true},handleAuthResult);
+}
+function handleAuthResult(authResult) {
+	if (authResult) {
+		// Access token has been successfully retrieved, requests can be sent to the API
+		loadClient(test);
+	} 
+	else {
+		redirect();
+	}
+}
+function loadClient(callback) {
+	gapi.client.load('drive', 'v2', callback);
+}
+function test() {
+	var request = gapi.client.drive.about.get();
+		request.execute(function(resp) {
+	});	
+	window.location.href = 'https://codeyourcloud.com'; 
+}
+function redirect(){
+	 window.location.href="https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile + https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive + https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.install + https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.file&state=%2Fprofile&redirect_uri=https%3A%2F%2Fcodeyourcloud.com&response_type=token&client_id=953350323460-0i28dhkj1hljs8m9ggvm3fbiv79cude6.apps.googleusercontent.com";
+ }
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-47415821-1', 'codeyourcloud.com');
+ga('send', 'pageview');
