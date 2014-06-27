c717301e41c155e09502470b514d1444e61ae76a
Michael Kaminsky <mkaminsky11@gmail.com>
2014-06-27 12:43:56 -0400

    various

--- scripts/features.js
+++ scripts/features.js
@@ -2,30 +2,20 @@ $(".code-li").each(function(index){
 	$(this).hover(function(){
 		$(this).animate({
 			backgroundColor: "#2980B9"
-		})
+		});
 	}, function(){
 		$(this).animate({
 			backgroundColor: "#3498DB"
-		})
+		});
 	});
 });
 
 $(".side-item").each(function(index){
 	$(this).hover(function(){
-		$(this).animate({
-			backgroundColor: "#83AEC9"
-		},{
-			duraction: 200,
-			queue: false
-		});
+		$(this).css("background-color", "#83AEC9");
 		
 	}, function(){
-		$(this).animate({
-			backgroundColor: "transparent"
-		},{
-			duraction: 200,
-			queue: false
-		});
+		$(this).css("background-color", "transparent");
 		
 	});
 });
